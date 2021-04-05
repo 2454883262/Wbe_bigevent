c71ca6ad377516d82f4017ec1ba7bd37e5211520
凌子轩 <2454883262@qq.com>
2021-04-05 19:42:53 +0800

    主页已完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,6 +3,23 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    console.log(options);
+    // console.log(options);
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+    // 统一为权限接口 设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 自动发起complete
+    options.complete = function(res) {
+        console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+        }
+    }
+
 })
