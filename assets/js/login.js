ce04a08a68b147dd4b639abc49159d10b01ff7ec
凌子轩 <2454883262@qq.com>
2021-04-05 15:37:15 +0800

    login页面完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,72 +1,89 @@
 $(function() {
+    // 点击“去注册账号”链接
     $('#link_reg').on('click', function() {
-
-        console.log(1);
         $('.login-box').hide().siblings().show();
-
     });
+    // 点击“去登录”链接
+
     $('#link_login').on('click', function() {
-
-        console.log(1);
         $('.reg-box').hide().siblings().show();
-
     });
-    var form = layui.form;
-    form.verify({
-        //'密码必须6到12位，且不能出现空格'
-        pwd: [/^[\S]{6,12}$/,
-            '密码必须6到12位，且不能出现空格'
-        ],
+
+
+
+    // layui中获取form对象
+    // 通过form.verify()函数自定义校验规则
+    layui.form.verify({
+        // 自定义密码规则
+        pwd: [/^\S{6,12}$/, '密码必须是6-12位，无空格'],
+        // 验证两次密码是否相同
         repwd: function(value) {
+            // 1.拿到密码框的值
+            // 2.判断值与密码框的值是否相同
+            // 3. 如果判断失败，return一个提示消息即可
             var pwd = $('.reg-box [name=password]').val();
-            console.log(pwd);
             if (pwd !== value) {
                 return '两次密码不一致';
             }
         }
-    });
-    // 监听注册表单的提交事件
+    })
+
+    // 监听注册表单提交事件
     $('#form_reg').on('submit', function(e) {
-        // 1. 阻止默认的提交行为
-        e.preventDefault()
-            // 2. 发起Ajax的POST请求
-        var data = {
-            username: $('#form_reg [name=username]').val(),
-            password: $('#form_reg [name=password]').val()
-        }
-        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function(res) {
-            if (res.status !== 0) {
-                return layer.msg(res.message)
-            }
-            layer.msg('注册成功，请登录！')
-                // 模拟人的点击行为
-            $('#link_login').click()
-        })
-    });
-    // 监听登录表单的提交事件
-    $('#form_login').submit(function(e) {
-        // 阻止默认提交行为
-        e.preventDefault()
+        e.preventDefault();
+        var pwd = $('.reg-box [name=password]').val();
+        var username = $('.reg-box [name=username]').val();
         $.ajax({
-            url: 'http://ajax.frontend.itheima.net/api/login',
             method: 'POST',
-            // 快速获取表单中的数据
-            data: $(this).serialize(),
+            url: '/api/reguser',
+            data: {
+                username: username,
+                password: pwd
+            },
             success: function(res) {
                 if (res.status !== 0) {
-                    return layer.msg('登录失败！')
+                    return layer.msg(res.message);
                 }
-                layer.msg('登录成功！')
-                    // 将登录成功得到的 token 字符串，保存到 localStorage 中
-                localStorage.setItem('token', res.token)
-                    // 跳转到后台主页
-                location.href = '/index.html'
+                layer.msg(res.message);
+
+                // 自动点击事件
+                $('#link_login').click();
             }
         })
-    });
-    $.ajaxPrefilter(function(options) {
-        // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-        options.url = 'http://ajax.frontend.itheima.net' + options.url
     })
+
+    // 监听登录表单的提交事件
+    $('#form_login').on('submit', function(e) {
+        e.preventDefault();
+        var pwd = $('.login-box [name=possword]');
+        var username = $('.login-box [name=username]');
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            data: $(this).serialize()
+                //  快速获取表单中的数据
+
+            // {
+            //     username: username,
+            //     password: pwd
+            // }
+            ,
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg(res.message);
+                // console.log(res.token);
+                // 登录成功得到token字符串 保存到localStorage
+                localStorage.setItem('token', res.token);
+                // 跳转到后台主页
+                location.href = '/index.html';
+            }
+
+        })
+
+    })
+
+
 
 })
